49145b073f451093335cb364779019acbf6a8ee8
Алексей Сергеевич <isip_a.s.opeykin@mpt.ru>
2022-04-21 09:18:43 +0300

    1

--- app/src/main/java/com/example/sqlite_news/MainActivity.java
+++ app/src/main/java/com/example/sqlite_news/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.sqlite_news;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +28,44 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
-            public void run() {
-                Intent intent;
-                intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finishAffinity();
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Log.e("ErrorAUTH", errString.toString());
             }
-        }, 3000);
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result){
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(MainActivity.this,"Успешно!",Toast.LENGTH_SHORT).show();
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent;
+                        intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        finishAffinity();
+                    }
+                }, 1500);
+            }
+
+            @Override
+            public void onAuthenticationFailed(){
+                super.onAuthenticationFailed();
+                Log.e("FailedAUTH", "FAIL!");
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Авторизация")
+                .setSubtitle("Прислоните палец")
+                .setNegativeButtonText("Отмена")
+                .build();
+
+        biometricPrompt.authenticate(promptInfo);
     }
 }
